ec0c4cf4e4c6b99e6fbaad70ee64b33a8bfa78c8
Tania <nusratjahantania111404@gmail.com>
2024-02-18 23:29:00 +0600

    third commit

--- script/script.js
+++ script/script.js
@@ -23,19 +23,62 @@ for(const seat of seats){
        //add seat number
        let addSeatText =document.getElementById("add-seat").innerText;
        let addSeat = parseInt(addSeatText);
-       console.log( addSeat);
+       //console.log( addSeat);
        addSeat = addSeat+1;
        document.getElementById("add-seat").innerText = addSeat;
 
        //set seat and price
        const setSeat = document.getElementById("set-seat");
-    //    const div = document.createElement('div');
-    //    div.classList.add("flex","gap-28","mt-6");
-    //    setSeat.appendChild(div);
-    const p = document.createElement('p');
-    p.innerText = 'tania';
-    setSeat.appendChild(p);
+       const div = document.createElement('div');
+        div.classList.add("flex","gap-28","mt-6");
+        setSeat.appendChild(div);
+    // const p = document.createElement('p');
+    // p.innerText = 'tania';
+    // setSeat.appendChild(p);
+       const p1 = document.createElement('p');
+       p1.innerText = seatName;
+       div.appendChild(p1);
+
+       const p2 = document.createElement('p');
+       p2.innerText = 'Economy';
+       div.appendChild(p2);
        
+       const p3 = document.createElement('p');
+       p3.innerText = '550';
+       div.appendChild(p3);
+
+       //calculate total price
+       const price = 550;
+       const totalPrice = price * count;
+       
+     //------------set price--------------
+     const priceText = document.getElementById('set-price').innerText;
+     document.getElementById('set-price').innerText =totalPrice;
+
+    //  ----------------coupon------------------------------
+    // const coupon = document.getElementById('coupon');
+    // const couponValue = coupon.value;
+    // console.log(couponValue);
+     
+    function check(){
+        const coupon = document.getElementById('coupon');
+    const couponValue = coupon.value.toUpperCase();
+    console.log(couponValue);
+
+    const button = document.getElementById('btn') =couponValue;
+
+     
+    if(couponValue == NEW15 || couponValue == COUPLE20){
+        console.log('you are right');
+    }
+    else{
+        console.log('you are wrong');
+    }
+    }
+
+
+     
+
 
     })
 }
